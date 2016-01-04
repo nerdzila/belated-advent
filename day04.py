5b8632f07214f71ee05a4ef9cc69f4faa65fa848
Alfonso Sanchez <nerdzila@gmail.com>
2016-01-04 11:05:50 -0600

    Adds answer to fourth day in python

---
+++ day04.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import hashlib
+
+ENCODING = 'utf-8'
+
+
+def hash_key_and_number(key, number):
+    m = hashlib.md5()
+    m.update(key.encode(ENCODING))
+    m.update(str(number).encode(ENCODING))
+    return m.hexdigest()
+
+
+def find_hash_seed(prefix, key):
+    current_number = 0
+
+    while True:
+        current_number += 1
+        current_hash = hash_key_and_number(puzzle_input, current_number)
+        if current_hash.startswith(prefix):
+            return current_number
+
+puzzle_input = input('Puzzle key: ')
+
+
+print(find_hash_seed('00000', puzzle_input))
+print(find_hash_seed('000000', puzzle_input))
