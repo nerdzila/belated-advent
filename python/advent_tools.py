10f51a11fc977b6ed2e055d2dba6062931b8eaed
Alfonso Sanchez <nerdzila@gmail.com>
2016-01-10 19:38:52 -0600

    Adds option to get input as iterable of lines

--- python/advent_tools.py
+++ python/advent_tools.py
@@ -3,18 +3,26 @@ import inspect
 from os import path
 
 
-def get_input_string():
-    caller_frame = inspect.currentframe().f_back
+def get_input_file_path():
+    caller_frame = inspect.currentframe().f_back.f_back
     caller_file = caller_frame.f_locals['__file__']
 
     input_file = path.basename(caller_file).replace('.py', '.in')
 
     cd = path.abspath(path.dirname(caller_file) + '/../input')
-    path_to_input = path.join(cd, input_file)
+    return path.join(cd, input_file)
 
-    with open(path_to_input, 'r') as input_file:
+
+def get_input_string():
+    with open(get_input_file_path(), 'r') as input_file:
         raw_string = input_file.read()
         return raw_string.strip('\n')
+
+
+def get_input_lines():
+    with open(get_input_file_path(), 'r') as input_file:
+        for line in input_file:
+            yield line
 
 
 def print_answer(which_answer, answer):
